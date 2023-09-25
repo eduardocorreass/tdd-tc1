7829433ecda3c92f7a2d3bcb03c54c2e936d84e8
Eduardo <eduardocorreass25@gmail.com>
2023-09-25 20:29:50 -0300

    feat: add interleaving logic

--- src/main/java/br/ifsp/edu/ArrayInterleaving.java
+++ src/main/java/br/ifsp/edu/ArrayInterleaving.java
@@ -3,8 +3,16 @@ package main.java.br.ifsp.edu;
 public class ArrayInterleaving {
     public int[] interleave(int[] a, int[] b){
 
-        if(a.length == 0 && b.length == 0) return new int[]{};
+        int[] interleavedArray = new int[a.length + b.length];
+        int j = 0;
 
-        return new int[]{a[0], b[0]};
+        for(int i = 0; i < 2 * a.length; i += 2){
+            interleavedArray[i] = a[j];
+            interleavedArray[i + 1] = b[j];
+
+            j++;
+        }
+
+        return interleavedArray;
     }
 }
