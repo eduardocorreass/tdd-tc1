93cbd0162801db2b3077e4a96436b6115a11b4ed
Eduardo <eduardocorreass25@gmail.com>
2023-09-25 20:10:44 -0300

    feat: add return of array when N = 1

--- src/main/java/br/ifsp/edu/ArrayInterleaving.java
+++ src/main/java/br/ifsp/edu/ArrayInterleaving.java
@@ -3,6 +3,8 @@ package main.java.br.ifsp.edu;
 public class ArrayInterleaving {
     public int[] interleave(int[] a, int[] b){
 
-        return new int[]{};
+        if(a.length == 0 && b.length == 0) return new int[]{};
+
+        return new int[]{a[0], b[0]};
     }
 }
