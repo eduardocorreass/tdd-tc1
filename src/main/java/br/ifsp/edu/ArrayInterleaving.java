eb01fa3ac1dd381f8ad129a39fc788f30cf61d3e
Eduardo <eduardocorreass25@gmail.com>
2023-09-25 21:17:29 -0300

    refactor: add N and change length to it

--- src/main/java/br/ifsp/edu/ArrayInterleaving.java
+++ src/main/java/br/ifsp/edu/ArrayInterleaving.java
@@ -1,12 +1,12 @@
 package main.java.br.ifsp.edu;
 
 public class ArrayInterleaving {
-    public int[] interleave(int[] a, int[] b){
+    public int[] interleave(int[] a, int[] b, int n){
 
-        int[] interleavedArray = new int[a.length + b.length];
+        int[] interleavedArray = new int[2 * n];
         int j = 0;
 
-        for(int i = 0; i < 2 * a.length; i += 2){
+        for(int i = 0; i < 2 * n; i += 2){
             interleavedArray[i] = a[j];
             interleavedArray[i + 1] = b[j];
 
