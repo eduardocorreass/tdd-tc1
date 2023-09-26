538b13734928cdff11d6f52431f8dacf91b436d1
Eduardo <eduardocorreass25@gmail.com>
2023-09-25 21:27:43 -0300

    feat: add N less than 0 verification

--- src/main/java/br/ifsp/edu/ArrayInterleaving.java
+++ src/main/java/br/ifsp/edu/ArrayInterleaving.java
@@ -3,6 +3,10 @@ package main.java.br.ifsp.edu;
 public class ArrayInterleaving {
     public int[] interleave(int[] a, int[] b, int n){
 
+        if (n < 0) {
+            throw new IllegalArgumentException("N must be greater or equal to 0");
+        }
+
         int[] interleavedArray = new int[2 * n];
         int j = 0;
 
