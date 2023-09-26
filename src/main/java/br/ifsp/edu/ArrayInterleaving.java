d05d900c66089b9653a6c9941f0396ab886fb9d7
Eduardo <eduardocorreass25@gmail.com>
2023-09-25 21:33:35 -0300

    feat: add A and B lengths verification

--- src/main/java/br/ifsp/edu/ArrayInterleaving.java
+++ src/main/java/br/ifsp/edu/ArrayInterleaving.java
@@ -6,6 +6,9 @@ public class ArrayInterleaving {
         if (n < 0) {
             throw new IllegalArgumentException("N must be greater or equal to 0");
         }
+        else if (a.length != n || b.length != n) {
+            throw new IllegalArgumentException("A and B lengths are not equal");
+        }
 
         int[] interleavedArray = new int[2 * n];
         int j = 0;
